2bf6761d0c7ee0a4486c87b958884039d4cb0bc8
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-23 10:41:57 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+// 실행하려면 node ~
+const http = require('http');
+const fs = require("fs");
+const path = require("path");
+
+function send404(response){
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: Resource not found.');
+  response.end();
+}
+
+const mimeLookup = {
+  '.js': 'application/javascript',
+  '.html': 'text/html',
+  '.css' : 'text/css'
+};
+
+const server = http.createServer((req, res) => {
+  if(req.method == 'GET'){
+
+    // let fileurl;
+    // if(req.url == '/'){
+    //   fileurl = 'index.html';
+    // } else {
+    //   fileurl = req.url;
+    // }
+    let filepath = path.resolve('./' + req.url);
+    let fileExt = path.extname(filepath);
+    let mimeType = mimeLookup[fileExt];
+
+    if(!mimeType) {
+      send404(res);
+      return;
+    }
+
+    fs.exists(filepath, (exists) => {
+      if(!exists){
+        send404(res);
+        return;
+      }
+
+      res.writeHead(200, {'Content-Type': mimeType});
+      fs.createReadStream(filepath).pipe(res);
+
+    });
+
+  }
+}).listen(3000);
+console.log("Server running at port 3000");
