2f413440e26e81f7f9cd4df1e5dd0cd224e28d69
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-24 10:37:51 +0900

    이벤트 버블링 사용하여 수정

--- example3/src/components/Items.js
+++ example3/src/components/Items.js
@@ -4,6 +4,7 @@ export default class Items extends Component {
     setup() {
         this.$state = {items: ['item1', 'item2']};
     }
+
     template() {
         const {items} = this.$state;
         return `
@@ -18,17 +19,36 @@ export default class Items extends Component {
             <button class="addBtn">추가</button>
         `
     }
+
     setEvent() {
-        this.$target.querySelector('.addBtn').addEventListener('click', () => {
-            const {items} = this.$state;
-            this.setState({ items: [...items, `item${items.length + 1}`] });
+
+        // 이벤트 버블링을 사용한 방식(?)
+        this.$target.addEventListener('click', ({ target }) => {
+            const items = [...this.$state.items];
+
+            if (target.classList.contains('addBtn')) {
+                this.setState({ items: [...items, `item${items.length + 1}`]});
+            }
+
+            if (target.classList.contains('deleteBtn')) {
+                items.splice(target.dataset.index, 1);
+                this.setState({items});
+            }
         });
 
-        this.$target.querySelectorAll('.deleteBtn').forEach(deleteBtn => 
-            deleteBtn.addEventListener('click', ({ target }) => {
-                const items = [...this.$state.items];
-                items.splice(target.dataset.index, 1);
-                this.setState({ items });
-        }));
+        
+        // this.$target.querySelector('.addBtn').addEventListener('click', () => {
+        //     const {items} = this.$state;
+        //     this.setState({ items: [...items, `item${items.length + 1}`] });
+        // });
+
+        // this.$target.querySelectorAll('.deleteBtn').forEach(deleteBtn => 
+        //     deleteBtn.addEventListener('click', ({ target }) => {
+        //         const items = [...this.$state.items];
+        //         items.splice(target.dataset.index, 1);
+        //         this.setState({ items });
+        // }));
+
+        
     }
 }
