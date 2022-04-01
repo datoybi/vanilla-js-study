d08b3a3c39ead7e6147937570ccc23e409d898d7
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-04-01 11:06:17 +0900

    컴포넌트로 추상화

--- status-system/example6/src/core/Component.js
+++ status-system/example6/src/core/Component.js
@@ -2,4 +2,25 @@ import { observable, observe } from './observer.js'
 
 export class Component {
     state; props; $el;
+
+    constructor ($el, props) {
+        this.$el = $el;
+        this.props = props;
+        this.setup();
+    }
+    
+    setup() {
+        this.state = observable(this.initState()); // state를 관찰한다.
+        observe(() => { // state가 변경될 경우, 함수가 실행된다.
+            this.render();
+            this.setEvent();
+            this.mounted();
+        });
+    }
+
+    initState() { return {} }
+    template() { return ''; }
+    render() { this.$el.innerHTML = this.template(); }
+    setEvent() {}
+    mounted() {}
 }
