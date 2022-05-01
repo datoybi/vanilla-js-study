30fba7ff41079db5160c258f27616ac000d5ed9a
dasom yun <dsy0302@gmail.com>
2022-05-01 21:20:15 +0900

    step2 요구사항 정리

--- moonbucks-menu/step2/src/js/index.js
+++ moonbucks-menu/step2/src/js/index.js
@@ -1,29 +1,34 @@
-// 첫번째 미션 - 문벅스 카페의 에스프레소 메뉴판 만들기
+// 두번째 미션 - 문벅스 카페의 메뉴판 여러개 만들기
 
-// 얻은 인사이트
-// 1. 이벤트 위임의 실제 사용법을 알게 되어서 좋았다.
-// 2. 요구 사항을 전략적으로 세세하게 나누는게 중요하다는 걸 알게 됐다.
-// 3. DOM 요소를 가져올 때 $표시를 써서 변수처럼 사용할 수 있는게 좋았다.
-// 4. 새롭게 알게 된 메서드 : Element.insertAdjacentHTML()
+// TODO localStorage Read/Write
+// - [] localStorage에 데이터를 저장한다.
+// - [] localStorage에 데이터를 읽어온다.
 
-// step1 요구사항 구현을 위한 전략
-// TODO 메뉴 추가
-// - [x] 메뉴의 이름을 입력 받고 엔터키를 누르면 메뉴가 추가된다.
-// - [x] 메뉴의 이름을 입력받고 확인 버튼을 클릭하면 메뉴를 추가한다.
-// - [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
-// - [x] 총 메뉴 개수를 count하여 상단에 보여준다.
-// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// TODO 카테고리별 메뉴판 관리
+// - [] 에스프레소 메뉴판을 관리를 만든다.
+// - [] 프라푸치노 메뉴판을 관리를 만든다.
+// - [] 블랜디드 메뉴판을 관리를 만든다.
+// - [] 티바나 메뉴판을 관리를 만든다.
+// - [] 디저트 메뉴판을 관리를 만든다.
 
-// TODO 메뉴 수정
-// - [x] 메뉴의 수정에 클릭이벤트를 받으면 prompt 인터페이스를 호출한다.
-// - [x] prompt창에 수정할 메뉴 명을 입력한 뒤, 확인 버튼을 누르면 메뉴가 수정된다.
+// TODO 페이지 접근시 최초 데이터 Read & Rendering
+// - [] 페이지에 최초로 접근할 때 localStorage의 에스프레소 메뉴를 읽어온다.
 
-// TODO 메뉴 삭제
-// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받으면, confirm 창을 호출한다.
-// - [x] 확인 버튼을 누르면 메뉴가 삭제된다.
+// TODO 품절 상태 관리
+// - [] 품절 버튼을 추가한다.
+// - [] 품절 버튼에 클릭 이벤트를 주어 sold-out class를 추가한다.
+// - [] 변경사항이 반영되도록 랜더링 해준다.
 
 const $ = (selector) => document.querySelector(selector);
+
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  setLocalStorage() {
+    localStorage.getItem("menu");
+  },
+};
 
 function App() {
   // form 태그가 자동으로 전송되는 걸 막아준다.
