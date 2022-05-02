b4c7844d9117a70e0ca0ed84d3fb5c6e4ad2b45b
unknown <dsy0302@gmail.com>
2022-05-02 13:46:44 +0900

    drap and drop 구현

--- etc/coordinate.js
+++ etc/coordinate.js
@@ -12,14 +12,22 @@
     
     screenX, screenY
         모니터 화면을 기준으로 좌표를 제공
-            
-        
-*/
-
+         
 window.addEventListener("click", function (e) {
   console.log("client: " + e.clientX, e.clientY);
   console.log("page: " + e.pageX, e.pageY);
   console.log("offset: " + e.offsetX, e.offsetY);
 });
-
 window.onload = () => {};
+
+
+
+- [] .resize 호버시 화살표 버튼 표출하기 
+- [] .resize 드래그 앤 드롭시 해당 width 가져오기
+- [] 가져온 width를 이용하여 myDiv도 같이 움직이게 하기
+
+document.querySelector("#treeDiv").offsetWidth 
+
+transform: translate(300px);
+
+*/
